cff7eaaf1cae0323c40c77adc4cf666938d4bbaf
Wistaster <wistaster@hotmail.com>
2023-09-13 03:36:30 +0300

    Web Api Added !

---
+++ WebAPI/Program.cs
@@ -0,0 +1,47 @@
+
+using Business.Abstracts;
+using Business.Conceretes;
+using DataAccess.Abstracts;
+using DataAccess.Conceretes.EntityFramework;
+
+namespace WebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            // Autofac, Ninject, CastleWindsor...
+            // StructureMap, LightInject, DryInject --> IoC
+            builder.Services.AddSingleton<IProductService,ProductManager>();
+            builder.Services.AddSingleton<IProductDal,EfProductDal>();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
